37a4bd3cb9f13528c88861acb81733b9c8e48b5a
jin young <jinnyxchoi@gmail.com>
2020-12-31 16:18:51 +0900

    Todo component added

---
+++ client/components/Todo/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Todo = () => {
+  console.log('hello')
+  return (
+    <div>
+      <p>TODO LISTS HERE</p>
+      <button type="submit">New List</button>
+    </div>
+  )
+}
