f8c52c2cc36c3004079786899f38e149916f7d52
jin young <jinnyxchoi@gmail.com>
2021-01-30 23:30:20 +0900

    calendar changes

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Todo} from './index'
+import Calendar from './Calendar/Calendar'
 
 /**
  * COMPONENT
@@ -13,6 +14,7 @@ export const UserHome = props => {
     <div>
       <h3>Welcome, {email}</h3>
       <Todo />
+      <Calendar />
     </div>
   )
 }
