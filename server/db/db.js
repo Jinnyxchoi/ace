87d2a0397b4bef7194a7be82fbdc74314228f682
jin young <jinnyxchoi@gmail.com>
2021-02-08 13:30:03 +0900

    dburl added

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const dbName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 
-var db = new Sequelize(databaseName, {
+var db = new Sequelize(dbUrl, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
