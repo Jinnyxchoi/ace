0c648cda06ee863f0cb6063816b32b8f80dea885
jin young <jinnyxchoi@gmail.com>
2021-02-08 14:05:43 +0900

    FINALLY FIXED ALL HEROKU BUGS

--- server/db/db.js
+++ server/db/db.js
@@ -10,7 +10,13 @@ var db = new Sequelize(dbUrl, {
     ssl: true
   }
 })
-
+// if seeding to local machine, since it won't take dialect options.
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   {
+//     logging: false
+//   }
+// )
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
