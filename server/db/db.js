41c3a2e33a281f17034a6715296d931c16c2418a
jin young <jinnyxchoi@gmail.com>
2021-03-04 19:07:52 +0900

    cleaning up code

--- server/db/db.js
+++ server/db/db.js
@@ -4,17 +4,17 @@ const pkg = require('../../package.json')
 const dbName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 
-const db = new Sequelize(dbUrl, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: true
-  }
-})
+// const db = new Sequelize(dbUrl, {
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: true
+//   }
+// })
 
 // if seeding to local machine, since it won't take dialect options.
-// const db = new Sequelize(dbUrl, {
-//   logging: false
-// })
+const db = new Sequelize(dbUrl, {
+  logging: false
+})
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
