ed57f5d024163df3b3081cdbea19bb267cb3686f
jin young <jinnyxchoi@gmail.com>
2021-03-06 17:22:22 +0900

    seed spec

--- script/seed.spec.js
+++ script/seed.spec.js
@@ -3,6 +3,18 @@
 
 const seed = require('./seed')
 
+const {Event} = require('../server/db/models')
+const {List} = require('../server/db/models')
+const {expect} = require('chai')
+
 describe('seed script', () => {
-  it('completes successfully', seed)
+  beforeEach(seed)
+  it('creates at least 3 events', async () => {
+    const events = await Event.findAll()
+    expect(events).to.have.lengthOf.at.least(3)
+  })
+  it('creates at least 3 lists', async () => {
+    const lists = await List.findAll()
+    expect(lists).to.have.lengthOf.at.least(3)
+  })
 })
--- server/db/db.js
+++ server/db/db.js
@@ -5,7 +5,7 @@ const dbName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 let db
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   db = new Sequelize(dbUrl, {
     logging: false
   })
